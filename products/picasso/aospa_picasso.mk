da3097a49d45701e4f8ba3fa139767993c782213
EndCredits <alicization.han@gmail.com>
2022-04-01 10:21:21 +0800

    aospa: Introduce Redmi K30 5G (picasso)

---
+++ products/picasso/aospa_picasso.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2022 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (aospa_picasso,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from picasso device
+$(call inherit-product, device/xiaomi/picasso/device.mk)
+
+# Inherit some common AOSPA stuff.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := derp_picasso
+PRODUCT_DEVICE := picasso
+PRODUCT_MODEL := Redmi K30 5G
+PRODUCT_BRAND := Redmi
+PRODUCT_MANUFACTURER := Redmi
+
+# Override device name for Play Store.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=picasso
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+endif
